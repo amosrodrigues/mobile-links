0ce4f019a11183ae56e65d0bee06dc161c1b37b0
Amós Rodrigues <amos.adm.rh@gmail.com>
2025-08-25 18:46:29 -0300

    feat: implement component category

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -4,6 +4,8 @@ import { MaterialIcons } from "@expo/vector-icons"
 import { styles } from "./style"
 import { colors } from "@/styles/colors"
 
+import { Category } from "@/components/category"
+
 export default function Index() {
   return (
     <View style={styles.container}>
@@ -14,6 +16,7 @@ export default function Index() {
           <MaterialIcons name="add" size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
+      <Category />
     </View>
   )
 }
