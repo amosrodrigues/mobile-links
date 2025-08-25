11e8bc9b812a91567c0b86faa427700261effd42
Amós Rodrigues <amos.adm.rh@gmail.com>
2025-08-25 19:45:37 -0300

    feat: create categories list components

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -3,9 +3,8 @@ import { MaterialIcons } from "@expo/vector-icons"
 
 import { styles } from "./style"
 import { colors } from "@/styles/colors"
-import { categories } from "@/utils/categories"
 
-import { Category } from "@/components/category"
+import { Categories } from "@/components/categories"
 
 export default function Index() {
   return (
@@ -18,13 +17,7 @@ export default function Index() {
         </TouchableOpacity>
       </View>
 
-      <Category
-        name={categories[1].name}
-        icon={categories[1].icon}
-        isSelected
-      />
-      <Category name="Site" icon="language" isSelected={false} />
-      <Category name="Video" icon="movie" isSelected={false} />
+      <Categories />
     </View>
   )
 }
