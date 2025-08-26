0f138774ddb7ec07aec6e27ecc17ed3abad05241
Amós Rodrigues <amos.adm.rh@gmail.com>
2025-08-25 23:55:25 -0300

    feat: implement navagation

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,14 +1,17 @@
 import { View, Text, TouchableOpacity } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
+import { router } from "expo-router"
 
 import { styles } from "./styles"
 import { colors } from "@/styles/colors"
+
+import { Categories } from "@/components/categories"
 
 export default function Add() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => router.back()}>
           <MaterialIcons name="arrow-back" size={32} color={colors.gray[200]} />
         </TouchableOpacity>
 
@@ -16,6 +19,7 @@ export default function Add() {
       </View>
 
       <Text style={styles.label}>Selecione uma categoria</Text>
+      <Categories />
     </View>
   )
 }
