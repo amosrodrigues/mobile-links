af9efa005d526494a3d50a1fab682e1ffceda657
Amós Rodrigues <amos.adm.rh@gmail.com>
2025-08-25 23:23:23 -0300

    feat: create component Option

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,4 +1,11 @@
-import { Image, View, TouchableOpacity, FlatList } from "react-native"
+import {
+  Text,
+  Image,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+} from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
 
 import { styles } from "./styles"
@@ -6,6 +13,7 @@ import { colors } from "@/styles/colors"
 
 import { Categories } from "@/components/categories"
 import { Link } from "@/components/link"
+import { Option } from "@/components/option"
 
 export default function Index() {
   return (
@@ -34,6 +42,40 @@ export default function Index() {
         contentContainerStyle={styles.linksContent}
         showsVerticalScrollIndicator={false}
       />
+
+      <Modal transparent visible={true}>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+              <TouchableOpacity>
+                <MaterialIcons
+                  name="close"
+                  size={20}
+                  color={colors.gray[400]}
+                />
+              </TouchableOpacity>
+            </View>
+
+            <Text style={styles.modalLinkName}>Rocketseat</Text>
+            <Text style={styles.modalUrl}>https://rocketseat.com.br</Text>
+
+            <View style={styles.modalFooter}>
+              <Option
+                name="Excluir"
+                icon="delete"
+                variant="secondary"
+                onPress={() => console.log("Excluir")}
+              />
+              <Option
+                name="Abrir"
+                icon="language"
+                onPress={() => console.log("Abrir")}
+              />
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   )
 }
