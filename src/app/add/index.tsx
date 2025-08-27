4933982e33b30a4236bb004f84c9c70b02f4157a
Amós Rodrigues <amos.adm.rh@gmail.com>
2025-08-27 16:17:21 -0300

    feat: implement validate inputs

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { View, Text, TouchableOpacity } from "react-native"
+import { View, Text, TouchableOpacity, Alert } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
 import { router } from "expo-router"
 
@@ -16,7 +16,9 @@ export default function Add() {
   const [url, setUrl] = useState("")
 
   function handleAdd() {
-    console.log({ name, url })
+    if (!category) return Alert.alert("Categoria", "Selecione a categoria")
+    if (!name.trim()) return Alert.alert("Nome", "Informe o nome")
+    if (!url.trim()) return Alert.alert("URL", "Informe a URL")
   }
 
   return (
@@ -34,7 +36,7 @@ export default function Add() {
 
       <View style={styles.form}>
         <Input placeholder="Nome" onChangeText={setName} autoCorrect={false} />
-        <Input placeholder="Url" onChangeText={setUrl} autoCorrect={false} />
+        <Input placeholder="URL" onChangeText={setUrl} autoCorrect={false} />
         <Button title="Adicionar" onPress={handleAdd} />
       </View>
     </View>
