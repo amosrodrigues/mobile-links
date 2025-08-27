3671182896072374fb66b6a0b46c52f3b39b7438
Amós Rodrigues <amos.adm.rh@gmail.com>
2025-08-27 18:24:00 -0300

    feat: implement redirect the user

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -28,6 +28,13 @@ export default function Add() {
         url,
         category,
       })
+
+      Alert.alert("Sucesso", "Novo link adicionado!", [
+        {
+          text: "Ok",
+          onPress: () => router.back(),
+        },
+      ])
     } catch (error) {
       Alert.alert("Erro", "Não foi possível salvar o link")
       console.log(error)
